c475e586b7d06be32e25967000187147e113ba55
tgiqfe <tqji31001@gmail.com>
2019-08-17 21:04:29 +0900

    Add Export-File

---
+++ PSFile/Cmdlet/ExportFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace PSFile.Cmdlet
+{
+    [Cmdlet(VerbsData.Export, "File")]
+    public class ExportFile : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Path { get; set; }
+        [Parameter]
+        public string Output { get; set; }
+        [Parameter]
+        [ValidateSet(Item.XML, Item.JSON, Item.YML)]
+        public string DataType { get; set; } = Item.JSON;
+
+        protected override void BeginProcessing()
+        {
+            DataType = Item.CheckCase(DataType);
+        }
+
+        protected override void ProcessRecord()
+        {
+            List<FileSummary> fsList = new List<FileSummary>();
+            fsList.Add(new FileSummary(Path, true));
+            if (Output == null)
+            {
+                DataSerializer.Serialize<List<FileSummary>>(fsList, Console.Out, DataType);
+            }
+            else
+            {
+                DataSerializer.Serialize<List<FileSummary>>(fsList, Output);
+            }
+        }
+    }
+}
---
+++ PSFile/Serialize/SerializableDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace PSFile
+{
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IXmlSerializable
+    {
+        public class KeyValue
+        {
+            public TKey Key { get; set; }
+            public TValue Value { get; set; }
+            public KeyValue() { }
+            public KeyValue(TKey key, TValue value)
+            {
+                this.Key = key;
+                this.Value = value;
+            }
+        }
+
+        public XmlSchema GetSchema() { return null; }
+
+        public void ReadXml(XmlReader reader)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(KeyValue));
+
+            reader.Read();
+            if (reader.IsEmptyElement) { return; }
+            while (reader.NodeType != XmlNodeType.EndElement)
+            {
+                KeyValue kv = serializer.Deserialize(reader) as KeyValue;
+                if (kv != null)
+                {
+                    Add(kv.Key, kv.Value);
+                }
+            }
+            reader.Read();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(KeyValue));
+            foreach (var key in Keys)
+            {
+                serializer.Serialize(writer, new KeyValue(key, this[key]),
+                    new XmlSerializerNamespaces(
+                        new XmlQualifiedName[1] { XmlQualifiedName.Empty }));
+            }
+        }
+    }
+}
