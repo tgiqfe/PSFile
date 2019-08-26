dd44c9579fa75c5dcb5df467291999486302bd6b
tgiqfe <tqji31001@gmail.com>
2019-08-26 17:53:18 +0900

    Add Open,Close-Registry

---
+++ PSFile/Cmdlet/Registry/CloseRegistry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Security.Principal;
+using System.Security.AccessControl;
+
+namespace PSFile.Cmdlet
+{
+    [Cmdlet(VerbsCommon.Close, "Registry")]
+    public class CloseRegistry : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Path { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            //  管理者実行確認
+            Message.CheckAdmin();
+
+            using (RegistryKey regKey = RegistryControl.GetRegistryKey(Path, false, false))
+            {
+                if (regKey == null) { return; }
+            }
+            string keyName = Path.Substring(Path.IndexOf("\\") + 1);
+            RegistryHive.UnLoad(keyName);
+
+            //  アンロード成功確認
+            using (RegistryKey regKey = RegistryControl.GetRegistryKey(Path, false, false))
+            {
+                if (regKey == null) { return; }
+            }
+
+            //  アンロード失敗時の再アンロード用コマンド
+            using (Process proc = new Process())
+            {
+                proc.StartInfo.FileName = "reg.exe";
+                proc.StartInfo.Arguments = $"unload \"{Path}\"";
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();
+            }
+        }
+    }
+}
---
+++ PSFile/Cmdlet/Registry/OpenRegistry.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Security.AccessControl;
+
+namespace PSFile.Cmdlet
+{
+    [Cmdlet(VerbsCommon.Open, "Registry")]
+    public class OpenRegistry : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Path { get; set; }
+        [Parameter]
+        public string File { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            //  管理者実行確認
+            Message.CheckAdmin();
+
+            using (RegistryKey regKey = RegistryControl.GetRegistryKey(Path, false, false))
+            {
+                if (regKey != null) { return; }
+            }
+            string keyName = Path.Substring(Path.IndexOf("\\") + 1);
+            RegistryHive.Load(keyName, File);
+
+            //  ロード成功確認
+            using (RegistryKey regKey = RegistryControl.GetRegistryKey(Path, false, false))
+            {
+                if (regKey != null)
+                {
+                    WriteObject(new RegistrySummary(Path));
+                    return;
+                }
+            }
+
+            //  ロード失敗時の再ロード用コマンド
+            using (Process proc = new Process())
+            {
+                proc.StartInfo.FileName = "reg.exe";
+                proc.StartInfo.Arguments = $"load \"{Path}\" \"{File}\"";
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();
+            }
+            using (RegistryKey regKey = RegistryControl.GetRegistryKey(Path, false, false))
+            {
+                if (regKey != null)
+                {
+                    WriteObject(new RegistrySummary(Path));
+                }
+            }
+        }
+    }
+}
